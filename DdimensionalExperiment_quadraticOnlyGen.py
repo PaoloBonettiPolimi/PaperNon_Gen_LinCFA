8f9207b85474f0101a7bb3c23f3005c21f304330
MacBook di Paolo <paolo@MBP-di-MacBook.homenet.telecomitalia.it>
2023-03-12 20:22:55 +0100

    squared only gen

--- DdimensionalExperiment_quadraticOnlyGen.py
+++ DdimensionalExperiment_quadraticOnlyGen.py
@@ -47,29 +47,29 @@ def squared_aggregation(x):
 def mean_aggregation(x):
     return x.mean(axis=1)
 
-def run_NonLinCFA(x,eps):
-    output = NonLinCFA_anyFunction(x.iloc[:2000,:],'target', eps, 5 , neigh=0, customFunction=mean_aggregation).compute_clusters()
+def run_NonLinCFA(x,eps,passed_fun):
+    output = NonLinCFA_anyFunction(x.iloc[:2000,:],'target', eps, 5 , neigh=0, customFunction=passed_fun).compute_clusters()
     
     aggregate_x = pd.DataFrame()
     for i in range(len(output)):
-        aggregate_x[i] = mean_aggregation(x[output[i]])
+        aggregate_x[i] = passed_fun(x[output[i]])
 
     actual_score = compute_r2(aggregate_x.iloc[:2000,:], x.loc[:,'target'].iloc[:2000], aggregate_x.iloc[2000:,:], x.loc[:,'target'].iloc[2000:])
     
     return [eps,len(output),actual_score]
 
-def run_GenLinCFA(x,eps):
-    output = GenLinCFA_anyFunction(x.iloc[:2000,:],'target', eps1=eps, n_val=5 , neigh=0, eps2=1, customFunction=mean_aggregation).compute_clusters()
+def run_GenLinCFA(x,eps,passed_fun):
+    output = GenLinCFA_anyFunction(x.iloc[:2000,:],'target', eps1=eps, n_val=5 , neigh=0, eps2=1, customFunction=passed_fun).compute_clusters()
 
     aggregate_x = pd.DataFrame()
     for i in range(len(output)):
-        aggregate_x[i] = mean_aggregation(x[output[i]])
+        aggregate_x[i] = passed_fun(x[output[i]])
         
     actual_score = compute_r2(aggregate_x.iloc[:2000,:], x.loc[:,'target'].iloc[:2000], aggregate_x.iloc[2000:,:], x.loc[:,'target'].iloc[2000:])
 
     return [eps,len(output),actual_score]
 
-def linear_experiment_noResampling(n_reps=10, n_variables=100, noise=10):
+def quadratic_experiment_noResampling(n_reps=10, n_variables=100, noise=10):
     NonLinCFA_score = [] 
     list_of_length = []
     wrapper_score = []
@@ -77,45 +77,30 @@ def linear_experiment_noResampling(n_reps=10, n_variables=100, noise=10):
     list_of_length_lin = []
     GenLinCFA_score = []
     list_of_length_gen = []
-    
-    x_all,y_all,coeffs = generate_dataset_n(n_data=3000*10, noise=noise, p1=0.3, p2=0.7, n_variables=n_variables, coeffs=[0])
+    NonLinCFA_score_sq = [] 
+    GenLinCFA_score_sq = []
+    wrapper_score_sq = []
+
+    x_all,y_all,coeffs = generate_dataset_squared(n_data=3000*10, noise=noise, p1=0.3, p2=0.7, n_variables=n_variables, coeffs=[0])
     
     for trials in range(n_reps):
         x = pd.DataFrame(x_all[3000*trials:3000*(trials+1)])
+        x_squared = x**2
         x['target'] = y_all[3000*trials:3000*(trials+1)]
-            
-        results = Parallel(n_jobs=10)(delayed(run_GenLinCFA)(x,eps) for eps in [0.76,0.77,0.78,0.79,0.80])#[0.375,0.4,0.4125,0.425,0.45])
+        x_squared['target'] = y_all[3000*trials:3000*(trials+1)]
+    
+        results = Parallel(n_jobs=10)(delayed(run_GenLinCFA)(x_squared,eps,mean_aggregation) for eps in [0.68,0.7,0.72,0.74,0.76])
         print(results)
         GenLinCFA_score.append(results)
 
-    return GenLinCFA_score
-
+        results = Parallel(n_jobs=10)(delayed(run_GenLinCFA)(x,eps,squared_aggregation) for eps in [0.68,0.7,0.72,0.74,0.76])
+        print(results)
+        GenLinCFA_score_sq.append(results)
+        
+    return GenLinCFA_score,GenLinCFA_score_sq
 
 ### generate a dataset of n samples and standardize the variables x1,x2,x3
-def generate_dataset_n(n_data=3000, noise=1, p1=0.5, p2=0.5, n_variables=10, coeffs=[0]):
-    x = np.zeros((n_data,n_variables))
-    
-    rng = np.random.default_rng(seed=0)
-    random.seed(30)
-
-    x[:,0] = rng.uniform(0,1,n_data)
-    if coeffs[0]==0:
-        coeffs = rng.uniform(0,1,size=n_variables)
-    
-    for i in range(n_variables-1):
-        j = random.randrange(i+1)
-        x[:,i+1] = p1*rng.uniform(0,1,n_data) + p2*x[:,j]
-    
-    scaler = preprocessing.StandardScaler().fit(x)
-    x = scaler.transform(x)
-    
-    delta = rng.normal(0, noise, size=(n_data,1))
-    
-    y = np.dot(x,coeffs).reshape(n_data,1) + delta.reshape(n_data,1)
-    return x,y,coeffs
-
-### generate a dataset of n samples with nonlinear relationship
-def generate_dataset_quadratic(n_data=3000, noise=1, p1=0.5, p2=0.5, n_variables=10, coeffs=[0]):
+def generate_dataset_squared(n_data=3000, noise=1, p1=0.5, p2=0.5, n_variables=10, coeffs=[0]):
     x = np.zeros((n_data,n_variables))
     
     rng = np.random.default_rng(seed=0)
@@ -137,7 +122,6 @@ def generate_dataset_quadratic(n_data=3000, noise=1, p1=0.5, p2=0.5, n_variables
     y = np.dot(x**2,coeffs).reshape(n_data,1) + delta.reshape(n_data,1)
     return x,y,coeffs
 
-
 ### main run
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -153,9 +137,9 @@ if __name__ == "__main__":
 
     ##################### experiment ########################
 
-    GenLinCFA_score = linear_experiment_noResampling(n_reps=args.n_repetitions, n_variables=args.n_variables, noise=args.noise)
+    GenLinCFA_score,GenLinCFA_score_sq = quadratic_experiment_noResampling(n_reps=args.n_repetitions, n_variables=args.n_variables, noise=args.noise)
     
     ##################### save the results ########################
     
     with open(args.results_file, 'wb') as f:  
-        pickle.dump([GenLinCFA_score], f)
+        pickle.dump([GenLinCFA_score,GenLinCFA_score_sq], f)
